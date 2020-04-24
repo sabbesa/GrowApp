87221cb7e00f02ae8160460be31cfd1eb5e77d5b
Sabina <sabina.tierp@gmail.com>
2020-04-24 12:21:29 +0200

    Lagt till styles till olika attribut.

--- Login.js
+++ Login.js
@@ -11,69 +11,76 @@ export default class Login extends React.Component {
   handleLogin = () => {
     const { email, password } = this.state
     firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => this.props.navigation.navigate('Main'))
-      .catch(error => this.setState({ errorMessage: error.message }))
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => this.props.navigation.navigate('Main'))
+    .catch(error => this.setState({ errorMessage: error.message }))
   }
 
 
   render() {
     return (
       <ImageBackground source={BackgroundYellow} style={styles.backgroundContainer}>
-        <View>
-          <Image source={Logo} style={styles.logo}/>
-        </View>
-        <Text>Login</Text>
-        {this.state.errorMessage &&
-          <Text style={{ color: 'red' }}>
-            {this.state.errorMessage}
-          </Text>}
+      <View>
+      <Image source={Logo} style={styles.logo}/>
+      </View>
+      <Text style={styles.Headline}>Login</Text>
+      {this.state.errorMessage &&
+        <Text style={{ color: 'red' }}>
+        {this.state.errorMessage}
+        </Text>}
         <TextInput
-          style={styles.textInput}
-          autoCapitalize="none"
-          placeholder="Email"
-          onChangeText={email => this.setState({ email })}
-          value={this.state.email}
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Email"
+        onChangeText={email => this.setState({ email })}
+        value={this.state.email}
         />
         <TextInput
-          secureTextEntry
-          style={styles.textInput}
-          autoCapitalize="none"
-          placeholder="Password"
-          onChangeText={password => this.setState({ password })}
-          value={this.state.password}
+        secureTextEntry
+        style={styles.textInput}
+        autoCapitalize="none"
+        placeholder="Password"
+        onChangeText={password => this.setState({ password })}
+        value={this.state.password}
         />
-        <Button title="Login" onPress={this.handleLogin} />
+        <Button title="Login"
+        style={styles.LoginButton}
+        onPress={this.handleLogin}
+        />
         <Button
-          title="Don't have an account? Sign Up"
-          onPress={() => this.props.navigation.navigate('SignUp')}
+        title="Don't have an account? Sign Up"
+        onPress={() => this.props.navigation.navigate('SignUp')}
         />
-      </ImageBackground>
-    )
+        </ImageBackground>
+      )
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  backgroundContainer: {
-    flex: 1,
-    width: null,
-    height: null,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  logo: {
-    height: 150,
-    width: 300,
-    position: 'relative'
+  const styles = StyleSheet.create({
+    backgroundContainer: {
+      flex: 1,
+      width: null,
+      height: null,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    logo: {
+      height: 150,
+      width: 300,
+      position: 'relative'
 
+    },
+    textInput: {
+      height: 50,
+      width: '90%',
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginTop: 8
+    },
+    LoginButton: {
+      height:70
 
-  },
-  textInput: {
-    height: 50,
-    width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 8
-  }
-})
+    },
+    Headline: {}
+  })
