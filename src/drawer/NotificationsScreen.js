51624919b79760f3a608cc93e789b59646b43175
xtinealexa <christinearkbo@gmail.com>
2020-05-19 13:58:17 +0200

    Mer chatt

--- src/drawer/NotificationsScreen.js
+++ src/drawer/NotificationsScreen.js
@@ -8,7 +8,7 @@ export class NotificationsScreen extends Component {
             <SafeAreaView style={{ flex: 1 }}>
             <CustomHeader title="Notifications" navigation={this.props.navigation}/>
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Chat Screen!1</Text>
+                <Text>Chat Screen!</Text>
             </View>
             </SafeAreaView>
         );
