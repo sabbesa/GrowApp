e919afcb03f014f87a0d9b8b196dcc999898c163
xtinealexa <christinearkbo@gmail.com>
2020-05-21 13:28:37 +0200

    Chatt

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+// @flow
+import React from 'react';
+import { GiftedChat } from 'react-native-gifted-chat'; // 0.3.0
 
-class Chat extends Component {
+import Fire from '../Fire';
+
+type Props = {
+  name?: string,
+};
+
+class Chat extends React.Component<Props> {
+
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || 'Chat!',
+  });
+
+  state = {
+    messages: [],
+  };
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
   render() {
-    return <View />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
+      />
+    );
+  }
+
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+  componentWillUnmount() {
+    Fire.shared.off();
   }
 }
-
-const styles = StyleSheet.create({});
 
 export default Chat;
