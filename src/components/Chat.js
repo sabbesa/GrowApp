fa311f68ba18c314742ee23c2996cb519e03b70b
Sabina <sabina.tierp@gmail.com>
2020-06-01 15:33:41 +0200

    Återställt chattfunktionen till forum

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,20 +1,8 @@
+// @flow
 import React from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  ImageBackground,
-  Image
-} from 'react-native';
-import PropTypes from 'prop-types';
-import Fire from '../../Fire';
-import Yellowfade from '../images/BackgroundYellow.png'
-import emojiUtils from 'emoji-utils';
+import { GiftedChat } from 'react-native-gifted-chat'; // 0.3.0
 
-import SlackMessage from './SlackMessage';
+import Fire from '../../Fire';
 
 type Props = {
   name?: string,
@@ -33,114 +21,179 @@ class Chat extends React.Component<Props> {
   get user() {
     return {
       name: this.props.route.params.name,
+
       _id: Fire.shared.uid,
     };
   }
 
   render() {
     return (
-      <View>
-      <ImageBackground source={Yellowfade} style={{width:'100%', height:'100%'}}>
       <GiftedChat
         messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
+        onSend={Fire.shared.send}
         user={this.user}
-        renderMessage={this.renderMessage}
       />
-      </ImageBackground>
-      </View>
     );
   }
 
-  // componentDidMount() {
-  //   Fire.shared.on(message =>
-  //     this.setState(previousState => ({
-  //       messages: GiftedChat.append(previousState.messages, message),
-  //     }))
-  //   );
-  // }
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hi! Please write your question and I will try to answer you shortly .',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'The Expert',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    })
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
   }
-  onSend(messages = []) {
-     this.setState(previousState => ({
-       messages: GiftedChat.append(previousState.messages, messages),
-     }))
-   }
-
-   renderMessage(props) {
-     const {
-       currentMessage: { text: currText },
-     } = props
-
-     let messageTextStyle
-
-     // Make "pure emoji" messages much bigger than plain text.
-     if (currText && emojiUtils.isPureEmojiString(currText)) {
-       messageTextStyle = {
-         fontSize: 28,
-         // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
-         lineHeight: Platform.OS === 'android' ? 34 : 30,
-       }
-     }
-
-     return <SlackMessage {...props} messageTextStyle={messageTextStyle} />
-   }
-
   componentWillUnmount() {
     Fire.shared.off();
   }
 }
 
-const styles = StyleSheet.create({
-  title: {
-    marginTop: offset,
-    marginLeft: offset,
-    fontSize: offset,
-  },
-  nameInput: {
-    height: offset * 2,
-    margin: offset,
-    paddingHorizontal: offset,
-    borderColor: '#111111',
-    borderWidth: 1,
-  },
-  chatInput: {
-    margin: offset,
-    backgroundColor: 'white',
-    borderWidth: 1,
-    marginLeft: 10,
-    paddingHorizontal: offset,
-  },
-  backgroundContainer: {
-        width:'100%',
-        height:'100%',
-      },
-  buttonText: {
-    marginTop: 50,
-    marginLeft: offset,
-    fontSize: offset,
-  },
-  welcomeText: {
-    marginTop: 50,
-    marginLeft: offset,
-    fontSize: offset,
-    fontWeight: 'bold',
-  },
-});
-
-const offset = 24;
 export default Chat;
+
+// import React from 'react';
+// import { GiftedChat } from 'react-native-gifted-chat';
+// import {
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   TouchableOpacity,
+//   View,
+//   ImageBackground,
+//   Image
+// } from 'react-native';
+// import PropTypes from 'prop-types';
+// import Fire from '../../Fire';
+// import Yellowfade from '../images/BackgroundYellow.png'
+// import emojiUtils from 'emoji-utils';
+//
+// import SlackMessage from './SlackMessage';
+//
+// type Props = {
+//   name?: string,
+// };
+//
+// class Chat extends React.Component<Props> {
+//
+//   static navigationOptions = ({ navigation }) => ({
+//     title: (navigation.state.params || {}).name || 'Chat!',
+//   });
+//
+//   state = {
+//     messages: [],
+//   };
+//
+//   get user() {
+//     return {
+//       name: this.props.route.params.name,
+//       _id: Fire.shared.uid,
+//     };
+//   }
+//
+//   render() {
+//     return (
+//       <View>
+//       <ImageBackground source={Yellowfade} style={{width:'100%', height:'100%'}}>
+//       <GiftedChat
+//         messages={this.state.messages}
+//         onSend={messages => this.onSend(messages)}
+//                //onSend={Fire.shared.send}
+//         user={this.user}
+//         renderMessage={this.renderMessage}
+//       />
+//       </ImageBackground>
+//       </View>
+//     );
+//   }
+//
+//   componentDidMount() {
+//     Fire.shared.on(message =>
+//       this.setState(previousState => ({
+//         messages: GiftedChat.append(previousState.messages, message),
+//       }))
+//     );
+//   }
+//   componentDidMount() {
+//     this.setState({
+//       messages: [
+//         {
+//           _id: 1,
+//           text: 'Hi! Please write your question and I will try to answer you shortly .',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'The Expert',
+//             avatar: 'https://placeimg.com/140/140/any',
+//           },
+//         },
+//       ],
+//     })
+//   }
+//   onSend(messages = []) {
+//      this.setState(previousState => ({
+//        messages: GiftedChat.append(previousState.messages, messages),
+//      }))
+//    }
+//
+//    renderMessage(props) {
+//      const {
+//        currentMessage: { text: currText },
+//      } = props
+//
+//      let messageTextStyle
+//
+//      // Make "pure emoji" messages much bigger than plain text.
+//      if (currText && emojiUtils.isPureEmojiString(currText)) {
+//        messageTextStyle = {
+//          fontSize: 28,
+//          // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+//          lineHeight: Platform.OS === 'android' ? 34 : 30,
+//        }
+//      }
+//
+//      return <SlackMessage {...props} messageTextStyle={messageTextStyle} />
+//    }
+//
+//   componentWillUnmount() {
+//     Fire.shared.off();
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   title: {
+//     marginTop: offset,
+//     marginLeft: offset,
+//     fontSize: offset,
+//   },
+//   nameInput: {
+//     height: offset * 2,
+//     margin: offset,
+//     paddingHorizontal: offset,
+//     borderColor: '#111111',
+//     borderWidth: 1,
+//   },
+//   chatInput: {
+//     margin: offset,
+//     backgroundColor: 'white',
+//     borderWidth: 1,
+//     marginLeft: 10,
+//     paddingHorizontal: offset,
+//   },
+//   backgroundContainer: {
+//         width:'100%',
+//         height:'100%',
+//       },
+//   buttonText: {
+//     marginTop: 50,
+//     marginLeft: offset,
+//     fontSize: offset,
+//   },
+//   welcomeText: {
+//     marginTop: 50,
+//     marginLeft: offset,
+//     fontSize: offset,
+//     fontWeight: 'bold',
+//   },
+// });
+//
+// const offset = 24;
+// export default Chat;
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,24 +5,15 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  ImageBackground,
-  Image
 } from 'react-native';
 
-import Yellowfade from '../images/BackgroundYellow.png'
-import firebase from '@react-native-firebase/app'
-
-
 class Main extends React.Component {
-
   static navigationOptions = {
     title: 'Chatter',
   };
 
   state = {
-
-   name: 'You',
-   currentUser: null,
+    name: '',
   };
 
   onPress = () =>
@@ -30,28 +21,19 @@ class Main extends React.Component {
 
   onChangeText = name => this.setState({ name });
 
-  componentDidMount() {
-    const { currentUser } = firebase.auth()
-
-
-    this.setState({ currentUser })
-  }
-
   render() {
-    const { currentUser } = this.state
     return (
       <View>
-      <ImageBackground source={Yellowfade} style={styles.backgroundContainer}>
-        <Text
-         style={styles.welcomeText}
-         onChangeText={this.onChangeText}
-          value={this.currentUser && this.currentUser.displayName}>
-          Hello {currentUser && currentUser.displayName}! Do you want to chat with the expert?
-        </Text>
+        <Text style={styles.title}>Enter your name:</Text>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="John Cena"
+          onChangeText={this.onChangeText}
+          value={this.state.name}
+        />
         <TouchableOpacity onPress={this.onPress}>
-          <Text style={styles.buttonText}>Press for chat</Text>
+          <Text style={styles.buttonText}>Next</Text>
         </TouchableOpacity>
-        </ImageBackground>
       </View>
     );
   }
@@ -65,28 +47,111 @@ const styles = StyleSheet.create({
     fontSize: offset,
   },
   nameInput: {
-  height: offset * 2,
+    height: offset * 2,
 
     margin: offset,
     paddingHorizontal: offset,
     borderColor: '#111111',
     borderWidth: 1,
   },
-  backgroundContainer: {
-        width:'100%',
-        height:'100%',
-      },
   buttonText: {
-    marginTop: 50,
     marginLeft: offset,
     fontSize: offset,
-  },
-  welcomeText: {
-    marginTop: 50,
-    marginLeft: offset,
-    fontSize: offset,
-    fontWeight: 'bold',
   },
 });
 
 export default Main;
+
+
+// import React from 'react';
+// import {
+//   StyleSheet,
+//   Text,
+//   TextInput,
+//   TouchableOpacity,
+//   View,
+//   ImageBackground,
+//   Image
+// } from 'react-native';
+//
+// import Yellowfade from '../images/BackgroundYellow.png'
+// import firebase from '@react-native-firebase/app'
+//
+//
+// class Main extends React.Component {
+//
+//   static navigationOptions = {
+//     title: 'Chatter',
+//   };
+//
+//   state = {
+//
+//    name: 'You',
+//    currentUser: null,
+//   };
+//
+//   onPress = () =>
+//     this.props.navigation.navigate('Chat', { name: this.state.name });
+//
+//   onChangeText = name => this.setState({ name });
+//
+//   componentDidMount() {
+//     const { currentUser } = firebase.auth()
+//
+//
+//     this.setState({ currentUser })
+//   }
+//
+//   render() {
+//     const { currentUser } = this.state
+//     return (
+//       <View>
+//       <ImageBackground source={Yellowfade} style={styles.backgroundContainer}>
+//         <Text
+//          style={styles.welcomeText}
+//          onChangeText={this.onChangeText}
+//           value={this.currentUser && this.currentUser.displayName}>
+//           Hello {currentUser && currentUser.displayName}! Do you want to chat with the expert?
+//         </Text>
+//         <TouchableOpacity onPress={this.onPress}>
+//           <Text style={styles.buttonText}>Press for chat</Text>
+//         </TouchableOpacity>
+//         </ImageBackground>
+//       </View>
+//     );
+//   }
+// }
+//
+// const offset = 24;
+// const styles = StyleSheet.create({
+//   title: {
+//     marginTop: offset,
+//     marginLeft: offset,
+//     fontSize: offset,
+//   },
+//   nameInput: {
+//   height: offset * 2,
+//
+//     margin: offset,
+//     paddingHorizontal: offset,
+//     borderColor: '#111111',
+//     borderWidth: 1,
+//   },
+//   backgroundContainer: {
+//         width:'100%',
+//         height:'100%',
+//       },
+//   buttonText: {
+//     marginTop: 50,
+//     marginLeft: offset,
+//     fontSize: offset,
+//   },
+//   welcomeText: {
+//     marginTop: 50,
+//     marginLeft: offset,
+//     fontSize: offset,
+//     fontWeight: 'bold',
+//   },
+// });
+//
+// export default Main;
